3195738f33c54875e103ebfbb6c202b628d75c58
najcoding <enoughnaj@gmail.com>
2025-12-14 14:07:03 +0000

    Add files via upload

---
+++ Print.py
@@ -0,0 +1,11 @@
+# Printing a greeting message
+print("Hello, world!")
+
+# Printing my name
+print("My name is Alex.")
+
+# Printing a funfact about myself
+print("I love to play football")
+
+# Performing and printing a simple calculator
+print("The result of 3 + 5 is:", 3 + 5)
